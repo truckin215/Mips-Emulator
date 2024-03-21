e029f21b969b0d8301f3a159b490558f6ed01e35
James Newman <n01490221@unf.edu>
2024-03-21 16:05:57 -0400

    Adjusted window size

--- src/main/java/dev/binarybrigade/mipsemulator/Main.java
+++ src/main/java/dev/binarybrigade/mipsemulator/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("EmulatorUI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
